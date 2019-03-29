bcbe60fba2ff3aa3163b1f95479a677fc84af215
MaximilianHille <47759191+MaximilianHille@users.noreply.github.com>
2019-03-29 14:13:57 +0100

    Update and rename db.js to DB.js

---
+++ JavaScript/DB.js
@@ -0,0 +1,17 @@
+function getSavedUsers() {
+    var users;
+    if (localStorage.getItem('users') === null) {
+        users = [
+            new User ("Maximilian", "Hille", "maximilian-hille@t-online.de", "mhille", "qwertz1234"),
+            new User ("Nadia", "Boujari", "nboujari@gmail.com", "nboujari", "1234qwertz"),
+            new User ("Kristina", "Bilkova", "kbilkova@gmail.com", "kbilkova", "qwerty1234"),
+        ];
+      } else {
+        users = JSON.parse(localStorage.getItem('users'));
+        for (let i = 0; i < users.length; i++) {
+          users[i] = new User(users[i].firstname, users[i].lastname, users[i].email, users[i].username, users[i].password);
+        }
+      }
+    return users
+  }
+  
--- JavaScript/db.js
+++
@@ -1,6 +0,0 @@
-var users = [
-    new User ("Maximilian", "Hille", "maximilian-hille@t-online.de", "mhille", "qwertz1234"),
-    new User ("Nadia", "Boujari", "nboujari@gmail.com", "nboujari", "1234qwertz"),
-    new User ("Kristina", "Bilkova", "kbilkova@gmail.com", "kbilkova", "qwerty5678"),
-];
-
