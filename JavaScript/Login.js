76b91fbeca8cbcfb14da97b0a480e5c05cc7c5b7
MaximilianHille <47759191+MaximilianHille@users.noreply.github.com>
2019-03-29 14:12:01 +0100

    Update Login.js

--- JavaScript/Login.js
+++ JavaScript/Login.js
@@ -1,33 +1,43 @@
-var registeredUser = [
-    {
-        username: "mhille",
-        password: "qwertz"
-    },
-    {
-        username: "kbilkova",
-        password: "qwerty"
-    },
-    {
-        username: "nboujari",
-        password: "1234qwertz"
-    }
-]
+var users = getSavedUsers()
 
 
 var login = document.getElementById("login")
 
-login.onclick = function existingUser () 
-{
-    var username = document.getElementById("username").value
-    var password = document.getElementById("password").value
+var counter = 3;
 
-    for (i = 0; i < registeredUser.length; i++) {
-        if(username === registeredUser[i].username && password === registeredUser[i].password) 
+
+if (login) 
+{
+    login.onclick = function () 
+    {
+        var username = document.getElementById("username").value
+        var password = document.getElementById("password").value
+    
+        for (i = 0; i < users.length; i++) {
+            if(username === users[i].username && password === users[i].password) 
+            {
+                alert("Welcome back " + username + "!")
+                document.location.href = "ActivityRegisteredUser.html";
+                return;
+            }
+        }  
+
+        if (counter == 0) 
         {
-            alert("Welcome back " + username)
-            document.location.href = "ActivityRegisteredUser.html";
-           return;
-        } 
+            alert("You've entered the wrong username and password three times. You`re serious?");
+            
+            document.getElementById("username").disabled = true;
+            document.getElementById("password").disabled = true;
+            login.disabled = true;
+
+            return false;
+         }
+         else 
+         {
+            alert("Wrong username or password! You have " + counter + " attempt(s) left!");
+            counter--;
+
+            }
     }
-    alert("Wrong username or password!")
 }
+ 
