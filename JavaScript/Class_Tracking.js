1f54c0eaa57cccf4ddbec47ca872be1ea9dfc0fc
MaximilianHille <47759191+MaximilianHille@users.noreply.github.com>
2019-04-06 13:59:53 +0200

    Update Class_Tracking.js

--- JavaScript/Class_Tracking.js
+++ JavaScript/Class_Tracking.js
@@ -1,1 +1,12 @@
+class Tracking {
 
+    // The constructor for our class, which will allow us to create new objects of our class
+    constructor(date, location, category, distance, finishTime) {
+        this.date = date;
+        this.location = location;
+        this.category = category;
+        this.distance = distance;
+        this.finishTime = finishTime;
+      
+    }
+}
