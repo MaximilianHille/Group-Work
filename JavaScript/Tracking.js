d97741f6c41507739be5977b8456f4def1d2358a
MaximilianHille <47759191+MaximilianHille@users.noreply.github.com>
2019-04-06 14:00:23 +0200

    Update Tracking.js

--- JavaScript/Tracking.js
+++ JavaScript/Tracking.js
@@ -1,89 +1,92 @@
 var rIndex,
 table = document.getElementById("table");
 
-// check the empty input
-function checkEmptyInput()
-{
-    var isEmpty = false,
-        date = document.getElementById("date").value,
-        location = document.getElementById("location").value,
-        category = document.getElementById("category").value,
-        distance = document.getElementById("distance").value,
-        finishTime = document.getElementById("finishTime").value;
+// check the empty input and in case it is true, return the alert that something needs to be filled in
+//also possible here to do instead of "else if" a "switch" statement!!
+      function checkEmptyInput()
+      {
+          var isEmpty = false,
+              date = document.getElementById("date").value,
+              location = document.getElementById("location").value,
+              category = document.getElementById("category").value,
+              distance = document.getElementById("distance").value,
+              finishTime = document.getElementById("finishTime").value;
 
-    if (date === ""){
-    alert ("Please fill in information");
-    isEmpty = true;
-    }
-    else if (location === "") {
-    alert ("Please fill in information");
-    isEmpty = true;
-    }
-    else if (category === ""){
-    alert ("Please fill in information");
-    isEmpty = true;
-    }
-    else if (distance === "") {
-    alert ("Please fill in information");
-    isEmpty = true;
-    }
-    else if (finishTime === "") {
-    alert ("Please fill in information");
-    isEmpty = true;
-    }
-    return isEmpty;
-    }
+          if (date === ""){
+          alert ("Please fill in information");
+          isEmpty = true;
+          }
+          else if (location === "") {
+          alert ("Please fill in information");
+          isEmpty = true;
+          }
+          else if (category === ""){
+          alert ("Please fill in information");
+          isEmpty = true;
+          }
+          else if (distance === "") {
+          alert ("Please fill in information");
+          isEmpty = true;
+          }
+          else if (finishTime === "") {
+          alert ("Please fill in information");
+          isEmpty = true;
+          }
+          return isEmpty;
+        }
 
-// add row
+// function to add a row to the HTML file
 function addHTMLTableRow()
 {
-// get the table by id 
-// create a new row and cells 
-// get value from input text
-//set the values into row cell`s
+      // get the table by its id 
+      // create a new row and cells 
+      // get value from input text
+      // set the values into row cell`s
 
-if (!checkEmptyInput()){
-var newRow = table.insertRow(table.length),
-cell1 = newRow.insertCell (0),
-cell2 = newRow.insertCell (1),
-cell3 = newRow.insertCell (2),
-cell4 = newRow.insertCell (3),
-cell5 = newRow.insertCell (4),
-date = document.getElementById("date").value,
-location = document.getElementById("location").value,
-category = document.getElementById("category").value,
-distance = document.getElementById("distance").value,
-finishTime = document.getElementById("finishTime").value;
+      if (!checkEmptyInput())
+      {
+        var newRow = table.insertRow(table.length),
+            cell1 = newRow.insertCell (0),
+            cell2 = newRow.insertCell (1),
+            cell3 = newRow.insertCell (2),
+            cell4 = newRow.insertCell (3),
+            cell5 = newRow.insertCell (4),
+            date = document.getElementById("date").value,
+            location = document.getElementById("location").value,
+            category = document.getElementById("category").value,
+            distance = document.getElementById("distance").value,
+            finishTime = document.getElementById("finishTime").value;
 
-cell1.innerHTML = date;
-cell2.innerHTML = location;
-cell3.innerHTML = category;
-cell4.innerHTML = distance;
-cell5.innerHTML = finishTime;
+            cell1.innerHTML = date;
+            cell2.innerHTML = location;
+            cell3.innerHTML = category;
+            cell4.innerHTML = distance;
+            cell5.innerHTML = finishTime;
 
-// call the function to set the event to the newrow
-selectedRowToInput();
+        // call the function to set the event to the new row --> basically we are adding out input here to the table
+        selectedRowToInput();
+      }
 }
-}
-// display selected row data into input text
+
+// this is the function in order to display selected row data into input text
 function selectedRowToInput()
 {
-for (var i = 0; i < table.rows.length; i++)
-{
-    table.rows[i].onclick = function ()
+    for (var i = 0; i < table.rows.length; i++)
     {
-        //get the selected row index
-        rIndex = this.rowIndex;
-        document.getElementById("date").value = this.cells[0].innerHTML;
-        document.getElementById("location").value = this.cells[1].innerHTML;
-        document.getElementById("category").value = this.cells[2].innerHTML;
-        document.getElementById("distance").value = this.cells[3].innerHTML;
-        document.getElementById("finishTime").value = this.cells[4].innerHTML;
+        table.rows[i].onclick = function ()
+        {
+            //get the selected row index
+            rIndex = this.rowIndex;
+            document.getElementById("date").value = this.cells[0].innerHTML;
+            document.getElementById("location").value = this.cells[1].innerHTML;
+            document.getElementById("category").value = this.cells[2].innerHTML;
+            document.getElementById("distance").value = this.cells[3].innerHTML;
+            document.getElementById("finishTime").value = this.cells[4].innerHTML;
 
-    };
+        };
+    }
 }
-
-}
+// call the function that it will do its work
 selectedRowToInput();
 
 function editHTMLTableSelectedRow()
@@ -93,26 +96,27 @@ function editHTMLTableSelectedRow()
         category = document.getElementById("category").value,
         distance = document.getElementById("distance").value,
         finishTime = document.getElementById("finishTime").value;
-if(!checkEmptyInput())
-{
-        table.rows[rIndex].cells[0].innerHTML = date;
-        table.rows[rIndex].cells[1].innerHTML = location;
-        table.rows[rIndex].cells[2].innerHTML = category;
-        table.rows[rIndex].cells[3].innerHTML = distance;
-        table.rows[rIndex].cells[4].innerHTML = finishTime;
-}
+
+    if(!checkEmptyInput())
+    {
+            table.rows[rIndex].cells[0].innerHTML = date;
+            table.rows[rIndex].cells[1].innerHTML = location;
+            table.rows[rIndex].cells[2].innerHTML = category;
+            table.rows[rIndex].cells[3].innerHTML = distance;
+            table.rows[rIndex].cells[4].innerHTML = finishTime;
+    }
 
 
 }
 
 function removeSelectedRow()
 {
-table.deleteRow(rIndex);
-// clear input text 
-document.getElementById("date").value = "";
-document.getElementById("location").value = "";
-document.getElementById("category").value = "";
-document.getElementById("distance").value = "";
-document.getElementById("finishTime").value = "";
+    table.deleteRow(rIndex);
+    // clear input text for the row which have been chosen by pushing the remove
+    document.getElementById("date").value = "";
+    document.getElementById("location").value = "";
+    document.getElementById("category").value = "";
+    document.getElementById("distance").value = "";
+    document.getElementById("finishTime").value = "";
 }
 
