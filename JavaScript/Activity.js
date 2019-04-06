8835ba46f2a70ab53713f5b5826236ee333d8c30
MaximilianHille <47759191+MaximilianHille@users.noreply.github.com>
2019-04-06 15:51:04 +0200

    Update Activity.js

--- JavaScript/Activity.js
+++ JavaScript/Activity.js
@@ -1,11 +1,19 @@
 document.getElementById('continue-button').onclick = function () {
+
     if (document.getElementById('tracking')) {
         document.location.href = "Tracking_Overview.html";
+        return true
     }
     else if (document.getElementById('race')) {
         document.location.href = "";
+        return true;
     }
     else if (document.getElementById('training')) {
         document.location.href ="";
+        return true;
+    }
+    else if (document.getElementById('choose')){
+        alert("Please choose an option.");
+        return true;
     }
 };
