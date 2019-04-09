ef9382362eefa89f7d5ac0612a6b08fa24f1963a
MaximilianHille <47759191+MaximilianHille@users.noreply.github.com>
2019-04-09 17:42:23 +0200

    Update Activity.js

--- JavaScript/Activity.js
+++ JavaScript/Activity.js
@@ -1,20 +1,16 @@
 var cont = document.getElementById('continue-button');
 
-    cont.onclick = function () {
+cont.onclick = function () {
         var selection = document.getElementById("selection").value;
-
             if (selection == "tracking") {
                 document.location.href = "Tracking.html";
             }
-
             else if (selection == "race") {
                 document.location.href = "Race.html";
             }
-
             else if (selection == "training") {
                 document.location.href = "Training.html";
             }
-
             else if (selection == "chooseOption") {
                 alert ("You have to choose what you want to do.")
             }
