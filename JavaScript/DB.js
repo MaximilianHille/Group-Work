0b7297706ab9e1bfa09d8ecaa3304fff92eb343d
MaximilianHille <47759191+MaximilianHille@users.noreply.github.com>
2019-04-11 17:06:09 +0200

    Update DB.js

--- JavaScript/DB.js
+++ JavaScript/DB.js
@@ -1,17 +1,31 @@
 function getSavedUsers() {
-    var users;
-        if (localStorage.getItem('users') === null) {
-            users = [
-                new User ("Maximilian", "Hille", "maximilian-hille@t-online.de", "mhille", "qwertz1234"),
-                new User ("Nadia", "Boujari", "nboujari@gmail.com", "nboujari", "1234qwertz"),
-                new User ("Kristina", "Bilkova", "kbilkova@gmail.com", "kbilkova", "qwerty1234"),
-            ];
-      } 
-        else {
-            users = JSON.parse(localStorage.getItem('users'));
-            for (let i = 0; i < users.length; i++) {
-              users[i] = new User(users[i].firstname, users[i].lastname, users[i].email, users[i].username, users[i].password);
-            }
-      }
-    return users
+  var users;
+  if (localStorage.getItem('users') === null) {
+    users = [
+      new User("Maximilian", "Hille", "maximilian-hille@t-online.de", "mhille", "qwertz1234", null),
+      new User("Nadia", "Boujari", "nboujari@gmail.com", "nboujari", "1234qwertz", null),
+      new User("Kristina", "Bilkova", "kbilkova@gmail.com", "kbilkova", "qwerty1234", null),
+    ];
   }
+  else {
+    users = JSON.parse(localStorage.getItem('users'));
+  }
+  return users
+}
+
+/*
+
+function getSavedTrackings() {
+  var trackings;
+  if (localStorage.getItem('currentUser') == activeUser && localStorage.getItem('trackings') === null) {
+    trackings = [
+      new Tracking("2.02.2019", "Amager", "Street", "21,1", "1:20:00", "Windy."),
+    ];
+  }
+  else {
+    trackings = JSON.parse(localStorage.getItem('trackings'));
+    }
+    return trackings
+}
+
+*/
