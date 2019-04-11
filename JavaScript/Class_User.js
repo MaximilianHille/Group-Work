7610b0fa88912fceb795aa07b91a61da2ce2900f
MaximilianHille <47759191+MaximilianHille@users.noreply.github.com>
2019-04-11 15:10:37 +0200

    Update Class_User.js

--- JavaScript/Class_User.js
+++ JavaScript/Class_User.js
@@ -1,10 +1,11 @@
 class User {
 // The constructor for our class, which will allow us to create new objects of our class
-    constructor(firstname, lastname, email, username, password) {
+    constructor(firstname, lastname, email, username, password, tracking) {
           this.firstname = firstname;
           this.lastname = lastname;
           this.email = email;
           this.username = username;
           this.password = password;
+          this.tracking = null;
     }   
 }
