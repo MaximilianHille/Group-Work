1a999f9c09cdcf41f8ba21af40df8f723ac347a6
MaximilianHille <47759191+MaximilianHille@users.noreply.github.com>
2019-04-11 17:07:10 +0200

    Update Tracking.js

--- JavaScript/Tracking.js
+++ JavaScript/Tracking.js
@@ -1,5 +1,20 @@
+var users = getSavedUsers();
+var trackings = getSavedTrackings();
+
 var rIndex;
 var table = document.getElementById("table");
+
+var currentUserTracking;
+
+for (var i = 0; i < users.length; i++) {
+    if (users[i].username == activeUser) {
+        currentUserTracking = users[i].tracking
+    }
+}
+
+if (currentUserTracking != null) {
+    table.innerHTML = currentUserTracking
+}
 
 var btn1 = document.getElementById('btn1');
 var btn2 = document.getElementById('btn2');
@@ -8,53 +23,53 @@ var btn3 = document.getElementById('btn3');
 // check the empty input and in case it is true, return the alert that something needs to be filled in
 // also possible here to do instead of "else if" a "switch" statement!!
 function checkEmptyInput() {
-          var isEmpty = false;
-          var date = document.getElementById("date").value;
-          var location = document.getElementById("location").value;
-          var category = document.getElementById("category").value;
-          var distance = document.getElementById("distance").value;
-          var finishTime = document.getElementById("finishTime").value;
-          var comments = document.getElementById("comments").value;
+    var isEmpty = false;
+    var date = document.getElementById("date").value;
+    var location = document.getElementById("location").value;
+    var category = document.getElementById("category").value;
+    var distance = document.getElementById("distance").value;
+    var finishTime = document.getElementById("finishTime").value;
+    var comments = document.getElementById("comments").value;
 
-            if (date === ""){
-                alert ("Please fill in information");
-                isEmpty = true;
-            }
-            else if (location === "") {
-                alert ("Please fill in information");
-                isEmpty = true;
-            }
-            else if (category === ""){
-                alert ("Please fill in information");
-                isEmpty = true;
-            }
-            else if (distance === "") {
-                alert ("Please fill in information");
-                isEmpty = true;
-            }
-            else if (finishTime === "") {
-                alert ("Please fill in information");
-                isEmpty = true;
-            }
-            else if (comments.length > 100) {
-                alert ("Comment too long!")
-                isEmpty = true;
-            }
-            return isEmpty;
+    if (date === "") {
+        alert("Please fill in information");
+        isEmpty = true;
+    }
+    else if (location === "") {
+        alert("Please fill in information");
+        isEmpty = true;
+    }
+    else if (category === "") {
+        alert("Please fill in information");
+        isEmpty = true;
+    }
+    else if (distance === "") {
+        alert("Please fill in information");
+        isEmpty = true;
+    }
+    else if (finishTime === "") {
+        alert("Please fill in information");
+        isEmpty = true;
+    }
+    else if (comments.length > 100) {
+        alert("Comment too long!")
+        isEmpty = true;
+    }
+    return isEmpty;
 }
 
 // function to add a row to the HTML file
 
-if(btn1) {
+if (btn1) {
     btn1.onclick = function addRow() {
         if (!checkEmptyInput()) {
             var newRow = table.insertRow(table.length);
-            var cell1 = newRow.insertCell (0);
-            var cell2 = newRow.insertCell (1);
-            var cell3 = newRow.insertCell (2);
-            var cell4 = newRow.insertCell (3);
-            var cell5 = newRow.insertCell (4);
-            var cell6 = newRow.insertCell (5);
+            var cell1 = newRow.insertCell(0);
+            var cell2 = newRow.insertCell(1);
+            var cell3 = newRow.insertCell(2);
+            var cell4 = newRow.insertCell(3);
+            var cell5 = newRow.insertCell(4);
+            var cell6 = newRow.insertCell(5);
 
             var date = document.getElementById("date").value;
             var location = document.getElementById("location").value;
@@ -63,25 +78,49 @@ if(btn1) {
             var finishTime = document.getElementById("finishTime").value;
             var comments = document.getElementById("comments").value;
 
-                cell1.innerHTML = date;
-                cell2.innerHTML = location;
-                cell3.innerHTML = category;
-                cell4.innerHTML = distance;
-                cell5.innerHTML = finishTime;
-                cell6.innerHTML = comments;
+            // var owner = getActiveUser();
 
-        // call the function to set the event to the new row --> basically we are adding out input here to the table
-        fillRow();
+            cell1.innerHTML = date;
+            cell2.innerHTML = location;
+            cell3.innerHTML = category;
+            cell4.innerHTML = distance;
+            cell5.innerHTML = finishTime;
+            cell6.innerHTML = comments;
+
+
+
+
+            // const newArray = []; 
+            // newArray.push(date, location, category, distance, finishTime, comments)
+            // console.log(newArray);
+            // call the function to set the event to the new row --> basically we are adding out input here to the table
+            fillRow();
         }
+
+        //trackings.push(users[i].tracking);
+        //localStorage.setItem('currentUser', JSON.stringify(trackings))
+
+        /*new activeUser (active)
+        new Tracking (date, location, category, distance, finishTime, comments));
+    localStorage.setItem('trackings', JSON.stringify(trackings)); */
+
+        /*activeUser = inputUsername.value
+        localStorage.setItem('currentUser', JSON.stringify(activeUser))
+
+
+        trackings.push(new Tracking(date, location, category, distance, finishTime, comments, owner));
+        localStorage.setItem("trackings", JSON.stringify(trackings)); */
+
+        // currentUserTracking = table.innerHTML;
+        // localStorage.setItem('Tracking', JSON.stringify(tracking));
+
     }
-}    
+}
 
-      // get the table by its id 
-      // create a new row and cells 
-      // get value from input text
-      // set the values into row cell`s
-
-      
+// get the table by its id 
+// create a new row and cells 
+// get value from input text
+// set the values into row cell`s
 
 // this is the function in order to display selected row data into input text
 function fillRow() {
@@ -98,11 +137,11 @@ function fillRow() {
 
         };
     }
-}   
+}
 // call the function that it will do its work
 fillRow();
 
-if(btn2) {
+if (btn2) {
     btn2.onclick = function editRow() {
         var date = document.getElementById("date").value;
         var location = document.getElementById("location").value;
@@ -111,7 +150,7 @@ if(btn2) {
         var finishTime = document.getElementById("finishTime").value;
         var comments = document.getElementById("comments").value;
 
-    if(!checkEmptyInput()) {
+        if (!checkEmptyInput()) {
             table.rows[rIndex].cells[0].innerHTML = date;
             table.rows[rIndex].cells[1].innerHTML = location;
             table.rows[rIndex].cells[2].innerHTML = category;
@@ -119,18 +158,24 @@ if(btn2) {
             table.rows[rIndex].cells[4].innerHTML = finishTime;
             table.rows[rIndex].cells[5].innerHTML = comments;
         }
+
+        //trackings.push(new Tracking(date, location, category, distance, finishTime, comments));
+        //    localStorage.setItem('trackimgs', JSON.stringify(trackings));
     }
 }
-    
-if(btn3) {
+
+if (btn3) {
     btn3.onclick = function removeRow() {
         table.deleteRow(rIndex);
-    // clear input text for the row which have been chosen by pushing the remove
-            document.getElementById("date").value = "";
-            document.getElementById("location").value = "";
-            document.getElementById("category").value = "";
-            document.getElementById("distance").value = "";
-            document.getElementById("finishTime").value = "";
-            document.getElementById("comments").value = "";
+        // clear input text for the row which have been chosen by pushing the remove
+        document.getElementById("date").value = "";
+        document.getElementById("location").value = "";
+        document.getElementById("category").value = "";
+        document.getElementById("distance").value = "";
+        document.getElementById("finishTime").value = "";
+        document.getElementById("comments").value = "";
     }
+
+    //trackings.push(new Tracking(date, location, category, distance, finishTime, comments));
+    //localStorage.setItem('trackimgs', JSON.stringify(trackings));
 }
