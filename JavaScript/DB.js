e254b4cfddc412e87cf2127c937b945c45e12a7a
MaximilianHille <47759191+MaximilianHille@users.noreply.github.com>
2019-04-24 20:32:16 +0200

    Update DB.js

--- JavaScript/DB.js
+++ JavaScript/DB.js
@@ -1,6 +1,6 @@
 function getSavedUsers() {
   var users;
-  if (localStorage.getItem('users') === null) {
+  if (localStorage.getItem('user') === null) {
     users = [
       new User("Maximilian", "Hille", "maximilian-hille@t-online.de", "mhille", "qwertz1234", null),
       new User("Nadia", "Boujari", "nboujari@gmail.com", "nboujari", "1234qwertz", null),
@@ -8,24 +8,7 @@ function getSavedUsers() {
     ];
   }
   else {
-    users = JSON.parse(localStorage.getItem('users'));
+    users = JSON.parse(localStorage.getItem('user'));
   }
   return users
 }
-
-/*
-
-function getSavedTrackings() {
-  var trackings;
-  if (localStorage.getItem('currentUser') == activeUser && localStorage.getItem('trackings') === null) {
-    trackings = [
-      new Tracking("2.02.2019", "Amager", "Street", "21,1", "1:20:00", "Windy."),
-    ];
-  }
-  else {
-    trackings = JSON.parse(localStorage.getItem('trackings'));
-    }
-    return trackings
-}
-
-*/
