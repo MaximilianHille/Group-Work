ebb6d7f2858fe8dc2a5deeaf97835ec0ae5fd6bb
MaximilianHille <47759191+MaximilianHille@users.noreply.github.com>
2019-04-25 11:10:10 +0200

    Update Class_User.js

--- JavaScript/Class_User.js
+++ JavaScript/Class_User.js
@@ -1,11 +1,18 @@
+// we are creating our user class for each user/new user
 class User {
+    
     // The constructor for our class, which will allow us to create new objects of our class
+    // this is giving us 2 options:
+    // 1. create hard coded users as seen in our database and assign them the properties written in the constructor
+    // 2. push new registered users and their properties into the user class as a new object
     constructor(firstname, lastname, email, username, password, tracking) {
         this.firstname = firstname;
         this.lastname = lastname;
         this.email = email;
         this.username = username;
         this.password = password;
+    // tracking is currently "null" because there is nothing inside yet
+    // as soon as trackings are saved to a current user they will be assigned to the tracking property
         this.tracking = null;
     }
 }
