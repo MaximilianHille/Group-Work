d867638e18d202b319ee7de058ca119bfeb1a74b
MaximilianHille <47759191+MaximilianHille@users.noreply.github.com>
2019-04-30 14:46:03 +0200

    Create Class_Tracking.js

---
+++ JavaScript/Class_Tracking.js
@@ -0,0 +1,11 @@
+class Tracking {
+    constructor(user, date, location, category, distance, finishTime, comments) {
+        this.activeUser = user;
+        this.date = date;
+        this.location = location;
+        this.category = category;
+        this.distance = distance;
+        this.finishTime = finishTime;
+        this.comments = comments;
+    }
+}
