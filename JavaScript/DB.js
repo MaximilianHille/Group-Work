8e59e1a523bd081d7f017f8dd0d675dd6d2936a7
MaximilianHille <47759191+MaximilianHille@users.noreply.github.com>
2019-04-30 14:45:00 +0200

    Update DB.js

--- JavaScript/DB.js
+++ JavaScript/DB.js
@@ -2,10 +2,10 @@
 // creating the function and giving it a name since we are using it on different JS files
 // it is the core for logging in or creating a new user
 function getSavedUsers() {
-  
+
   // we are creating again a variable called "users" and leave it empty for now until the next step
   var users;
-  
+
   // we are having 2 steps here:
   // on the one hand we are in the "if" retrieving the data from the local storage
   // if this data is empty (with the key word "user") we are retrieving our hard coded users with their data inside
@@ -17,13 +17,13 @@ function getSavedUsers() {
       new User("Kristina", "Bilkova", "kbilkova@gmail.com", "kbilkova", "qwerty1234", null),
     ];
   }
-  
+
   // on the other hand as soon as a person registered, we will always carry out this part of the function and
   // retrieve all the users from the local storage
   else {
     users = JSON.parse(localStorage.getItem('user'));
   }
-  
+
   // giving us back the users which are registered
   return users
 }
